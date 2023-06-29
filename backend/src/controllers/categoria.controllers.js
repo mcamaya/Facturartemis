194bced07daeedc6d9d4bd084999fefc6af1627c
mcamaya <camiamayar@gmail.com>
2023-06-29 11:55:57 -0500

    refactor: :hammer: Re-estructuración del controlador para evitar código repetido

--- backend/src/controllers/categoria.controllers.js
+++ backend/src/controllers/categoria.controllers.js
@@ -1,27 +1,24 @@
-import getConnection from "../db/database.js"
+import getConnection from "../db/database.js";
+
+const structure = async (res, sqlQuery) => {
+    const connection = await getConnection();
+    const result = await connection.query(...sqlQuery);
+    res.json(result);
+}
 
 const getCategorias = async (req, res) => {
     try {
-
-        const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM categorias");
-        res.json(result);
-
+        structure(res, ["SELECT * FROM categorias"]);
     } catch (error) {
         res.status(500);
         res.send(error.message);
     }
 }
-
 
 const getUnicaCategoria = async (req, res) => {
     try {
-        console.log(req.params);
         const {id} = req.params;
-
-        const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM categorias WHERE CategoriaID = ?", id);
-        res.json(result);
+        structure(res, ["SELECT * FROM categorias WHERE CategoriaID = ?", id]);
 
     } catch (error) {
         res.status(500);
@@ -29,36 +26,13 @@ const getUnicaCategoria = async (req, res) => {
     }
 }
 
-const deleteCategoria = async (req, res) => {
-    try {
-        const {id} = req.params;
-
-        const connection = await getConnection();
-        const result = await connection.query("DELETE FROM categorias WHERE CategoriaID = ?", id);
-        res.json(result);
-
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);
-    }
-}
 
 const postCategorias = async (req, res) => {
     try {
-        /* console.log(req.body); */
         const {CategoriaNombre, Descripcion, Imagen} = req.body;
-        const nuevaCategoria = {
-            CategoriaNombre,
-            Descripcion,
-            Imagen
-        }
-
-        const connection = await getConnection();
-
-        const result = await connection.query("INSERT INTO categorias SET ?", nuevaCategoria);
-
-        res.json(result);
-
+        const nuevaCategoria = {CategoriaNombre, Descripcion, Imagen}
+        structure(res, ["INSERT INTO categorias SET ?", nuevaCategoria]);
+        
     } catch (err) {
         res.status(500);
         res.send(err.message);
@@ -70,16 +44,21 @@ const updateCategorias = async (req, res) => {
         const {id} = req.params;
         const {CategoriaNombre, Descripcion, Imagen} = req.body;
         const categoriaActualizada = {CategoriaNombre, Descripcion, Imagen}
-
-        const connection = await getConnection();
-
-        const result = await connection.query("UPDATE categorias SET ? WHERE CategoriaID = ?", [categoriaActualizada, id]);
-
-        res.json(result);
-
+        structure(res, ["UPDATE categorias SET ? WHERE CategoriaID = ?", [categoriaActualizada, id]]);
+        
     } catch (err) {
         res.status(500);
         res.send(err.message);
+    }
+}
+
+const deleteCategoria = async (req, res) => {
+    try {
+        const {id} = req.params;
+        structure(res, ["DELETE FROM categorias WHERE CategoriaID = ?", id]);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
     }
 }
 
